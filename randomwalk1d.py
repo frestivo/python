aeaedbbb69befcd7ef3ed45b126bdd93a1a5105b
frestivo <frestivo@gmail.com>
2020-08-17 01:20:52 +0100

    Create randomwalk1d.py
    As is, simulates one million random movements along a line, stating at 0 and indication the final position. Plots the histogram of the used positions

---
+++ randomwalk1d.py
@@ -0,0 +1,21 @@
+import random
+import matplotlib.pyplot as plt
+histo = {}
+pos = 0
+count = 0
+while count < 1000000:
+  pos = pos + 2*random.randint(0, 1) - 1
+  if pos not in histo:
+    histo[pos] = 1
+  else:
+    histo[pos] += 1
+  count += 1
+# display
+x = []
+y = []
+for i in sorted(histo):
+  x.append(i)
+  y.append(histo[i])
+plt.plot(x, y)
+plt.title('Última posição ' + str(pos))
+plt.show()
