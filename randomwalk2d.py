c4ae99288dd5419cead74693f1663f13c722f919
frestivo <frestivo@gmail.com>
2020-08-23 19:01:02 +0100

    Update randomwalk2d.py
    Now in three colours.

--- randomwalk2d.py
+++ randomwalk2d.py
@@ -21,6 +21,11 @@ for i in range(1, n):
         x[i] = x[i - 1]
         y[i] = y[i - 1] - 1
 #plot
-plt.title('Última posição ' + str(x[n - 1]) + ', ' + str(y[n - 1]))
-plt.plot(x, y)
+plt.figure(figsize=(8, 8))
+plt.title('Última posição: ' + str(x[n - 1]) + ', ' + str(y[n - 1]))
+plt.plot(x[0 : n//3], y[0 : n//3], 'orangered')
+plt.plot(x[n//3 : 2*n//3], y[n//3 : 2*n//3], 'olivedrab')
+plt.plot(x[2*n//3 : n], y[2*n//3 : n], 'skyblue')
+plt.plot(0, 0, 'o', color='black')
+plt.plot(x[n - 1], y[n - 1], 'o', color='black')
 plt.show()
